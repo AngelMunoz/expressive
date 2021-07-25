7d07bf09c3be5a154705c570c34f6cd125bce7f7
Angel D. Munoz <scythes.lol@gmail.com>
2021-07-25 18:06:00 +0000

    add files

---
+++ Program.fs
@@ -0,0 +1,27 @@
+module Expressive.Program
+
+open System
+open Glutinum.Express
+open Glutinum.ExpressServeStaticCore
+open Fable.Core.JsInterop
+
+let bodyParser =
+    importDefault<Glutinum.BodyParser.BodyParser.IExports> "body-parser"
+
+let app = express.express ()
+
+app.``use`` (bodyParser.json ())
+
+app.get ("/", (fun (req: Request) (res: Response) -> res.send {| message = "Hello Fable!" |} |> ignore))
+
+
+
+// Define a function to construct a message to print
+let from whom = sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    app.listen (3000, "0.0.0.0", (fun _ -> printfn "Started app at 0.0.0.0:3000"))
+    |> ignore
+
+    0 // return an integer exit code
