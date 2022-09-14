2e44f6b30b30b5030dba3b4e6e5292f2b284d54b
Angel D. Munoz <scythes.lol@gmail.com>
2022-09-14 09:48:15 -0500

    update to fable 4

--- Program.fs
+++ Program.fs
@@ -14,11 +14,6 @@ app.``use`` (bodyParser.json ())
 
 app.get ("/", (fun (req: Request) (res: Response) -> res.send {| message = "Hello Fable!" |} |> ignore))
 
-
-
-// Define a function to construct a message to print
-let from whom = sprintf "from %s" whom
-
 [<EntryPoint>]
 let main argv =
     app.listen (3000, "0.0.0.0", (fun _ -> printfn "Started app at 0.0.0.0:3000"))
